9d73484db74a168ae58930cb18c60abf88961b54
Daniel <kronkowski.daniel@gmail.com>
2022-12-18 20:55:36 +0100

    first class 'building'

--- santa_village.py
+++ santa_village.py
@@ -1,5 +1,14 @@
 
-class village():
-    pass
+class village_building():
+    
+    def __init__(self, name, purpose):
+        self.name = name 
+        self.purpose = purpose
 
-class 
+    def __str__(self):
+        return f"{self.name} is Santa Clause Village building and intended for {self.purpose}."
+
+    def tenant(self, tenand="Santa Clause"):
+        self.tenand = tenand
+        return f"In {self.name} you can find {self.tenand}"
+
