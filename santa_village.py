72e4fe20c2cc648ec1b774bbe7d399bcc6362408
Daniel <kronkowski.daniel@gmail.com>
2022-12-19 21:18:58 +0100

    Added desciption of residents (tenant)

--- santa_village.py
+++ santa_village.py
@@ -8,7 +8,34 @@ class village_building():
     def __str__(self):
         return f"{self.name} is Santa Clause Village building and intended for {self.purpose}."
 
-    def tenant(self, tenand="Santa Clause"):
-        self.tenand = tenand
-        return f"In {self.name} you can find {self.tenand}"
+
+class tentant():
+    
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def __str__(self):
+        return f"This is {self.name} a {self.age} years old {self.type}"
+
+class Elf(tentant):
+    
+    def attrribute(self, ability = "hard-working"):
+        self.ability = ability
+
+class Reindeer(tenant):
+
+    def attrribute(self, ability = "hungry"):
+        self.ability = ability
+
+class Santa_Clause(tenant):
+
+    def attrribute(self, ability = "busy"):
+        self.ability = ability
+
+class Mrs_Santa(tenant):
+
+    def attrribute(self, ability = "caring"):
+        self.ability = ability
 
